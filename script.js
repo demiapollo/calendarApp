cd95b681b9c0c9795495b7e585e5542e8c2ba0ab
demiapollo <apollodemirel67@gmail.com>
2023-03-12 20:14:29 -0700

    color classes are added.

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT:
-  // What does `this` reference in the click listener function?
-  // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked?
-  // How might the id be useful when saving the description in local storage?
-
   //I added the current time and day right under h1 and p tags
   let currentTimeDay = dayjs();
   $("#currentDay").text(currentTimeDay.format("MM/DD/YYYY"));
-
-  // let timePicked = parseInt($(this).parent().attr("id"));
 
   //click event listener for the save button
   $(".saveBtn").on("click", function () {
@@ -22,8 +9,9 @@ $(function () {
 
     // // timePicked is a number now
     let timePicked = parseInt($(this).parent().attr("id"));
-    console.log(timePicked);
-    console.log(typeof timePicked);
+    // console.log(timePicked);
+    // console.log(typeof timePicked);
+
     //save in local storage
     localStorage.setItem(timePicked, eventCreated);
   });
@@ -32,10 +20,6 @@ $(function () {
   let currentHour = parseInt(dayjs().format("HH"));
 
   function checkTime() {
-    // let hourPicked = parseInt($(this).parent().attr("id"));
-    // console.log(typeof hourPicked);
-    // console.log(hourPicked);
-
     $(".time-block").each(function () {
       let timePicked = parseInt($(this).attr("id"));
 
@@ -55,9 +39,16 @@ $(function () {
     });
   }
 
-  checkTime();
+  //get user input from localStorage
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  checkTime();
 });
